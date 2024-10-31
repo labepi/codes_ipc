dbef43d4ff1b81424f7970e9c32fc703b5f72af3
Joao Borges <joaoborges6@gmail.com>
2024-10-31 12:31:39 -0300

    Atualizando funcoes pub/sub Paho MQTT Python

--- pubsub/temperature/publisher.py
+++ pubsub/temperature/publisher.py
@@ -1,21 +1,26 @@
-import paho.mqtt.client as mosquitto
 import time
+
+import paho.mqtt.client as mqtt
 
 # https://pypi.org/project/paho-mqtt/
 
+
 # callbacks
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc, properties):
     if rc == 0:
         print("Connected")
 
-def on_disconnect(client, userdata, rc):
+
+def on_disconnect(client, userdata, flags, rc, properties):
     print("Disconnected successfully.")
 
-def on_publish(client, userdata, mid):
-    print("Message ",str(mid)," published.")
+
+def on_publish(client, userdata, mid, reason_code, properties):
+    print("Message ", str(mid), " published.")
+
 
 # creating a client
-client = mosquitto.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 # setting callbacks
 client.on_connect = on_connect
@@ -23,21 +28,21 @@ client.on_disconnect = on_disconnect
 client.on_publish = on_publish
 
 # connecting to the broker
-#connect(host, port=1883, keepalive=60, bind_address="")
+# connect(host, port=1883, keepalive=60, bind_address="")
 client.connect(host="127.0.0.1", port=1883)
 
 # starting the looping thread
-client.loop_start()
+# client.loop_start()
 
 # publishing to the topic
-client.publish(topic="sensors/temperature", payload="50", qos=0, retain=False)
+client.publish(topic="sensors/temperature", payload="50", qos=0,
+               retain=False)
 
 # NOTE: publish an empty payload to clear retained messages
-#client.publish(topic="sensors/temperature", payload="", qos=0, retain=True)
+# client.publish(topic="sensors/temperature", payload="", qos=0, retain=True)
 
 # disconnecting
 client.disconnect()
 
 # stoping the looping thread
-client.loop_stop(force=False)
-
+client.loop_stop()
--- pubsub/temperature/subscriber.py
+++ pubsub/temperature/subscriber.py
@@ -1,17 +1,26 @@
-import paho.mqtt.client as mosquitto
+import signal
+
+import paho.mqtt.client as mqtt
 
 # https://pypi.org/project/paho-mqtt/
 
+
 # callbacks
-def on_connect(client, userdata, flags, rc):
+###########
+
+
+def on_connect(client, userdata, flags, rc, properties):
     if rc == 0:
         print("Connected successfully")
 
-def on_disconnect(client, userdata, rc):
+
+def on_disconnect(client, userdata, flags, rc, properties):
     print("Disconnected successfully.")
 
-def on_subscribe(client, userdata, flags, rc):
+
+def on_subscribe(client, userdata, flags, rc, properties):
     print("Subscribed to the topic")
+
 
 def on_message(client, userdata, msg):
     print("msg topic: " + str(msg.topic))
@@ -19,8 +28,22 @@ def on_message(client, userdata, msg):
     print("msg qos: " + str(msg.qos))
     print("--")
 
+
+# finalizando
+def quit(signum, frame):
+    print("Finalizando...")
+    # disconnecting
+    client.disconnect()
+    # stoping the looping thread
+    client.loop_stop()
+    exit(0)
+
+
+# associando a funcao para tratar o Ctrl+C
+signal.signal(signal.SIGINT, quit)
+
 # creating a client
-client = mosquitto.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 # setting callbacks
 client.on_connect = on_connect
@@ -29,21 +52,18 @@ client.on_subscribe = on_subscribe
 client.on_message = on_message
 
 # connecting to the broker
-client.connect(host="127.0.0.1", port=1883, keepalive=60, bind_address="")
+client.connect(host="127.0.0.1", port=1883, keepalive=60)  # , bind_address="")
+# client.connect("127.0.0.1", 1883, 60)  # , bind_address="")
 
 # subscribing to the topic
 client.subscribe(topic="sensors/temperature", qos=1)
 # quality of service:
-# 0: The broker/client will deliver the message once, 
+# 0: The broker/client will deliver the message once,
 #    with no confirmation.
-# 1: The broker/client will deliver the message  at  
+# 1: The broker/client will deliver the message  at
 #    least  once,  with confirmation required.
-# 2: The broker/client will deliver the message exactly 
+# 2: The broker/client will deliver the message exactly
 #    once by using a four step handshake
 
 # starting the looping thread
 client.loop_forever()
-
-# disconnecting
-client.disconnect()
-
